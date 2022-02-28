0d1a08e73df454c457bff7217ab3be1812fcd45b
Vignesh <vemcse@gmail.com>
2022-03-01 00:13:59 +0530

    adding x days

--- init.rb
+++ init.rb
@@ -5,10 +5,22 @@ Redmine::Plugin.register :new_features do
   version '0.0.1'
   Redmine::WikiFormatting::Macros.register do
     macro :timelog_table do |obj,args|
-	    return '' if obj.blank?
-	    project = obj.is_a?(Issue) ? obj.project : obj.issue.project
+      if obj.blank?
+        if self.assigns.present? && self.assigns["project"].is_a?(Project)
+          project = self.assigns["project"]
+        else
+          return ''
+        end
+      elsif obj.is_a?(Issue)
+        project = obj.project
+      elsif obj.is_a?(Journal)
+        project = obj.issue.project
+      else
+        return ''
+      end
+      x_time = Time.now - args.first.to_i.days
 	    activity = TimeEntryActivity.where(name: 'Internal training').first
-	    time_entries = project.time_entries.where.not(activity_id: activity.try(:id)).group_by(&:user)
+	    time_entries = project.time_entries.where("created_on >= ? ", x_time).where.not(activity_id: activity.try(:id)).group_by(&:user)
       html = "<table><th>User Name</th><th>Issue</th><th>Title</th><th>Total time</th>"
       time_entries.each do |user, entries|
 	      entry_group = entries.group_by(&:issue)
